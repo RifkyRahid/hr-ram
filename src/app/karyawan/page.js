545eff413b599dc69f8627c0d43769a0e6355ccb
Rifky Rahid <rifky29042002@gmail.com>
2024-01-25 15:43:31 +0700

    update card

--- src/app/karyawan/page.js
+++ src/app/karyawan/page.js
@@ -26,10 +26,34 @@ const page = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} flexDirection={"row"}>
           <Header />
           <DrawerHeader />
           
+          <Grid item sx={12} sm={6}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
           <Grid item sx={12} sm={6}>
             <Card sx={{ maxWidth: 345 }}>
               <CardMedia
