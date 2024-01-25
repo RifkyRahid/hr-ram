bfeeea771881dce5c54caf2a577e361c2a5a7c4f
Rifky Rahid <rifky29042002@gmail.com>
2024-01-25 13:42:14 +0700

    feat: membuat card di /karyawan

--- src/app/karyawan/page.js
+++ src/app/karyawan/page.js
@@ -1,7 +1,62 @@
+"use client";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  styled,
+} from "@mui/material";
 import React from "react";
+import Header from "../components/Header";
 
-export default function card() {
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const page = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <Box sx={{ display: "flex" }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Grid container spacing={3}>
+          <Header />
+          <DrawerHeader />
+          
+          <Grid item sx={12} sm={6}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default page;
