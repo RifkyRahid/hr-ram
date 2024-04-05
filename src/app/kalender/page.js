9aad1beda6ee7ec200c168d0152a7483eeb76eea
Rifky Rahid <rifky29042002@gmail.com>
2024-04-05 14:15:39 +0700

    feat: add calendar component and use it at kalendar.js

--- src/app/kalender/page.js
+++ src/app/kalender/page.js
@@ -1,9 +1,9 @@
 "use client";
 import * as React from "react";
 import Header from "../components/Header";
+import Calendar from "../components/Calendar";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-
 
 
 
@@ -17,77 +17,28 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   // marginTop: "64px", // Menambahkan margin atas yang sama dengan AppBar
 }));
 
+const CenteredText = styled("div")({
+  textAlign: "center", // Menengahkan teks secara horizontal
+});
+
+const StyledPaper = styled(Paper)({
+  position: "absolute",
+  top: "55%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+});
 
 export default function StickyHeadTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
     <Paper sx={{ width: "80%", overflow: "hidden" }}>
       <CenteredText>
         <Header />
         <DrawerHeader />
-        <StyledPaper sx={{ width: '80%', maxHeight: '80vh', overflow: 'auto' }}>  
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.code}
-                    >
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+        <StyledPaper sx={{ width: '80%', maxHeight: '90vh', overflow: 'auto' }}>  
+        <Calendar />
         </StyledPaper>
       </CenteredText>
     </Paper>
